b12ae67df41d891931061bfa8f921518ac4c44c4
h <hirakawa.n@gmail.com>
2015-07-25 17:45:22 +0900

    Initial Commit

---
+++ GLTelemetryPict/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GLTelemetryPict
+//
+//  Created by h on 7/25/15.
+//  Copyright (c) 2015 h. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ GLTelemetryPict/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  GLTelemetryPict
+//
+//  Created by h on 7/25/15.
+//  Copyright (c) 2015 h. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
