610548db2f6b4fa7343c26c3b41d4d3eb2cd0134
h <hirakawa.n@gmail.com>
2015-07-28 05:58:15 +0900

    png連結用のターゲット追加。書き出せますがメモリ積んでない人はトライしないように。。

---
+++ ImageCouple/ImageCouple/AppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.m
+//  ImageCouple
+//
+//  Created by h on 7/28/15.
+//  Copyright (c) 2015 h. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+@end
